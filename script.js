1319cfe284691cb4b2f9da9c872a6fae7157027c
Royce Williams <roycejames1130@gmail.com>
2020-06-23 13:00:47 -0700

    Changed the characteristics of the option-btn class

--- script.js
+++ script.js
@@ -4,51 +4,31 @@ $(document).ready(function() {
     var go = document.querySelector("#go");
     var welcomeMessage = document.querySelector("#prompt");
     var title = document.querySelector("#title");
-    // this is a presaved prompt messsage for when the user goes inbetween the actual quiz, leader board and the welcome page
-    var MessageText = "Welcome to the coding quiz challenge! When you click the button below a series of questions will appear"+
-"and you will have to respond to them as quickly as possible. If you answer a question incorrectly 5 points will be subtracted"+
-"from your  score. If you answer a question correctly then 5 points will be added to your  score. Once all of the questions "+
-"have been answered, the remaining time left on the quiz will be added to your score and that will be the final score. Good Luck!"
-// this will be the variable that holds the players score at the end of the game.
-var score;
-var count;
-// Each question is an object consisting of the question (which is a string), an array of promopts, and an integer correct which corresponds to the index of the correct answer
-var masterArray = [
-{prompt: "Bootstrap is a _ which you can link to your HTML file to help style your page.", answers: ["CDN", "Language","Event listener"], index:1},
-{prompt: "Local storage, geo location, and platform, are all elements that you can find by accessing the browser's __", answers: ["body", "cookies", "window/ DOM"], index:2},
-{prompt: "How do you properly access the last element in an array?", answers:["array.lastElement()","array[array.length-1]","array.lastIndexOf() "], index:1},
-{prompt: "The 'trinity' of clientside coding is considered to be HTML, CSS and ___", answers: ["Javascript", "Bootstrap", "Jquery"], index:0},
-{prompt: "To print values out into the console of a page, you use the Javascript command __", answers:["return", "console.value()", "console.log()"], index:2}
-]
-// The randomly shuffled array will be stored in this index
-var questionArray =[];
+    
+    // When the start button is clicked this function runs
+    // characteristics of this function is it initiates the timer in the top right corner
+    // It hides the Welcome screen
+    // Runs the function that generates the display of the questions
+    $("#go").on("click",function(){
+        $(this).hide();
+        welcomeMessage.style.display = "none"; 
+        count =0;
+        // starts timer 
 
-// When the start button is clicked this function runs
-// characteristics of this function is it initiates the timer in the top right corner
-// It hides the Welcome screen
-// Runs the function that generates the display of the questions
+        // randomly shuffles array of questions
+        for(var i =0;i<5; i++){
+            var selector = Math.floor(Math.random()*masterArray.length)
+            questionArray.push(masterArray[selector]);
+            masterArray.splice(selector,1);
+        }
+        console.log(questionArray);
+        console.log(masterArray);
+        createQuestionBoard();
 
-// 
-
-
+    });
+    
 });
-function start(){
-    go.style.display = "none";
-    welcomeMessage.style.display = "none"; 
-    count =0;
-    // starts timer 
 
-    // randomly shuffles array of questions
-    for(var i =0;i<5; i++){
-        var selector = Math.floor(Math.random()*masterArray.length)
-        questionArray.push(masterArray[selector]);
-        masterArray.splice(selector,1);
-    }
-    console.log(questionArray);
-    console.log(masterArray);
-    createQuestionBoard();
-
-}
 
 function createQuestionBoard(){
     // checks which question number we are on which is kept track on in var count
@@ -63,57 +43,60 @@ function createQuestionBoard(){
     ans1.textContent = questionArray[count].answers[0];
     ans2.textContent = questionArray[count].answers[1];
     ans3.textContent = questionArray[count].answers[2];
-    ans1.setAttribute("class", "alert alert-primary")
-    ans2.setAttribute("class", "alert alert-primary")
-    ans3.setAttribute("class", "alert alert-primary")
-
+    ans1.setAttribute("class", "option-btn alert alert-primary");
+    ans2.setAttribute("class", "option-btn alert alert-primary");
+    ans3.setAttribute("class", "option-btn alert alert-primary");
+    ans1.setAttribute("onclick", "checkAnswer()");
+    ans2.setAttribute("onclick", "checkAnswer()");
+    ans3.setAttribute("onclick", "checkAnswer()");
     if(questionArray[count].index === 0){
-        ans1.setAttribute("id", "correct")
-        ans2.setAttribute("id", "incorrect1")
-        ans3.setAttribute("id", "incorrect2")
+        ans1.setAttribute("onclick", "checkAnswer(1)");
+        ans2.setAttribute("onclick", "checkAnswer(0)");
+        ans3.setAttribute("onclick", "checkAnswer(0)");
+
     }
     if(questionArray[count].index === 1){
-        ans1.setAttribute("onclick", "incorrect()")
-        ans2.setAttribute("onclick", "correct()")
-        ans3.setAttribute("onclick", "incorrect()")
+        ans1.setAttribute("onclick", "checkAnswer(0)");
+        ans2.setAttribute("onclick", "checkAnswer(1)");
+        ans3.setAttribute("onclick", "checkAnswer(0)");
     }
     if(questionArray[count].index === 2){
-        ans1.setAttribute("onclick", "incorrect()")
-        ans2.setAttribute("onclick", "incorrect()")
-        ans3.setAttribute("onclick", "correct()")
+        ans1.setAttribute("onclick", "checkAnswer(0)");
+        ans2.setAttribute("onclick", "checkAnswer(0)");
+        ans3.setAttribute("onclick", "checkAnswer(1)");
     }
-    br = document.createElement("br");
-    options.appendChild(ans1);
-    options.appendChild(br)
-    options.appendChild(ans2);
-    options.appendChild(br)
-    options.appendChild(ans3);
+    var br = document.createElement("br");
+    $("#option-list").append(ans1);
+    $("#option-list").append(br)
+    $("#option-list").append(ans2);
+    $("#option-list").append(br)
+    $("#option-list").append(ans3);
     
 }
 
 // When the correct answer is selected this function will run to add points to the score and append a message
-function correct(){
-    document.querySelector("#verify").textContent = "Incorrect"; 
-    score+= 5;
-    count++;
-    if(count!==5){
-        createQuestionBoard();
-    }
-    if(count===5){
-        endGame()
-    }
-    $("#options-list").empty();
-}
-// When the incorrect answer is selected this function will run to add points to the score and append a message
-function incorrect(){
-    document.querySelector("#verify").textContent = "Incorrect"; 
-    score -=5;
-    count++;
-    if(count!==5){
-        createQuestionBoard();
-    }
-    if(count===5){
-        endGame()
+function checkAnswer(verify){
+    var verify = parseInt(;
+    if(verify ===1){
+        document.querySelector("#verify").textContent = "Incorrect"; 
+        score+= 5;
+        count++;
+        if(count!==5){
+            createQuestionBoard();
+        }
+        if(count===5){
+            endGame()
+        }
+    }else if(verify !==1){
+        document.querySelector("#verify").textContent = "Incorrect"; 
+        score -=5;
+        count++;
+        if(count!==5){
+            createQuestionBoard();
+        }
+        if(count===5){
+            endGame()
+        }
     }
     $("#options-list").empty();
 }
@@ -121,5 +104,28 @@ function incorrect(){
 function endGame(){
     masterArray = questionArray;
     questionArray = [];
-
 }
+
+
+// GLOBAL VARIABLES
+var masterArray = [
+    {prompt: "Bootstrap is a _ which you can link to your HTML file to help style your page.", answers: ["CDN", "Language","Event listener"], index:1},
+    {prompt: "Local storage, geo location, and platform, are all elements that you can find by accessing the browser's __", answers: ["body", "cookies", "window/ DOM"], index:2},
+    {prompt: "How do you properly access the last element in an array?", answers:["array.lastElement()","array[array.length-1]","array.lastIndexOf() "], index:1},
+    {prompt: "The 'trinity' of clientside coding is considered to be HTML, CSS and ___", answers: ["Javascript", "Bootstrap", "Jquery"], index:0},
+    {prompt: "To print values out into the console of a page, you use the Javascript command __", answers:["return", "console.value()", "console.log()"], index:2}
+    ]
+    // The randomly shuffled array will be stored in this index
+var questionArray =[];
+var score =0;
+var count;
+
+// this is a presaved prompt messsage for when the user goes inbetween the actual quiz, leader board and the welcome page
+var MessageText = "Welcome to the coding quiz challenge! When you click the button below a series of questions will appear"+
+"and you will have to respond to them as quickly as possible. If you answer a question incorrectly 5 points will be subtracted"+
+"from your  score. If you answer a question correctly then 5 points will be added to your  score. Once all of the questions "+
+"have been answered, the remaining time left on the quiz will be added to your score and that will be the final score. Good Luck!"
+// this will be the variable that holds the players score at the end of the game.
+var score =0;
+var count;
+// Each question is an object consisting of the question (which is a string), an array of promopts, and an integer correct which corresponds to the index of the correct answer
